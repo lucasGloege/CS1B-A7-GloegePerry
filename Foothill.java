9521c32ea1358be9304a78ad276cb04b38dab9ad
LucasGloege <49991682+lucasGloege@users.noreply.github.com>
2020-11-25 13:57:26 -0800

    Added Classes from Lectures

--- Foothill.java
+++ Foothill.java
@@ -1,16 +1,346 @@
+import java.text.*;
+import java.util.*;
 
+//------------------------------------------------------
 public class Foothill
-{
-
-   public Foothill()
+{ 
+   // -------  main --------------
+   public static void main(String[] args) throws Exception
    {
-      // TODO Auto-generated constructor stub
+      FHtree<String> sceneTree = new FHtree<String>();
+      FHtreeNode<String> tn;
+      
+      // create a scene in a room
+      tn = sceneTree.addChild(null, "room");
+
+      // add three objects to the scene tree
+      sceneTree.addChild(tn, "Lily the canine");
+      sceneTree.addChild(tn, "Miguel the human");
+      sceneTree.addChild(tn, "table");
+      // add some parts to Miguel
+      tn = sceneTree.find("Miguel the human");
+
+      // Miguel's left arm
+      tn = sceneTree.addChild(tn, "torso");
+      tn = sceneTree.addChild(tn, "left arm");
+      tn =  sceneTree.addChild(tn, "left hand");
+      sceneTree.addChild(tn, "thumb");
+      sceneTree.addChild(tn, "index finger");
+      sceneTree.addChild(tn, "middle finger");
+      sceneTree.addChild(tn, "ring finger");
+      sceneTree.addChild(tn, "pinky");
+
+      // Miguel's right arm
+      tn = sceneTree.find("Miguel the human");
+      tn = sceneTree.find(tn, "torso", 0);
+      tn = sceneTree.addChild(tn, "right arm");
+      tn =  sceneTree.addChild(tn, "right hand");
+      sceneTree.addChild(tn, "thumb");
+      sceneTree.addChild(tn, "index finger");
+      sceneTree.addChild(tn, "middle finger");
+      sceneTree.addChild(tn, "ring finger");
+      sceneTree.addChild(tn, "pinky");
+
+      // add some parts to Lily
+      tn = sceneTree.find("Lily the canine");
+      tn = sceneTree.addChild(tn, "torso");
+      sceneTree.addChild(tn, "right front paw");
+      sceneTree.addChild(tn, "left front paw");
+      sceneTree.addChild(tn, "right rear paw");
+      sceneTree.addChild(tn, "left rear paw");
+      sceneTree.addChild(tn, "spare mutant paw");
+      sceneTree.addChild(tn, "wagging tail");
+
+      // add some parts to table
+      tn = sceneTree.find("table");
+      sceneTree.addChild(tn, "north east leg");
+      sceneTree.addChild(tn, "north west leg");
+      sceneTree.addChild(tn, "south east leg");
+      sceneTree.addChild(tn, "south west leg");
+
+      sceneTree.display();
+      
+      sceneTree.remove("spare mutant paw");
+      sceneTree.remove("Miguel the human");
+      sceneTree.remove("an imagined higgs boson");
+      
+      sceneTree.display();
    }
-
-   public static void main(String[] args)
-   {
-      // TODO Auto-generated method stub
-
-   }
-
 }
+
+class FHtreeNode<E>
+{
+   // use protected access so the tree, in the same package, 
+   // or derived classes can access members  
+   protected FHtreeNode<E> firstChild, sib, prev;
+   protected E data;
+   protected FHtreeNode<E> myRoot;  // needed to test for certain error
+
+   public FHtreeNode( E d, FHtreeNode<E> sb, FHtreeNode<E> chld, FHtreeNode<E> prv )
+   {
+      firstChild = chld; 
+      sib = sb;
+      prev = prv;
+      data = d;
+      myRoot = null;
+   }
+   
+   public FHtreeNode()
+   {
+      this(null, null, null, null);
+   }
+   
+   public E getData() { return data; }
+
+   // for use only by FHtree (default access)
+   protected FHtreeNode( E d, FHtreeNode<E> sb, FHtreeNode<E> chld, 
+      FHtreeNode<E> prv, FHtreeNode<E> root )
+   {
+      this(d, sb, chld, prv);
+      myRoot = root;
+   }
+}
+
+class FHtree<E> implements Cloneable
+{
+   private int mSize;
+   FHtreeNode<E> mRoot;
+  
+   protected boolean deleted; // true if the node has been removed from the tree
+   
+   public FHtree() { clear(); }
+   public boolean empty() { return (mSize == 0); }
+   public int size() { return mSize; }
+   public void clear() { mSize = 0; mRoot = null; }
+   
+   public FHtreeNode<E> find(E x) { return find(mRoot, x, 0); }
+   public boolean remove(E x) { return remove(mRoot, x); }
+   public void  display()  { display(mRoot, 0); }
+   
+   public < F extends Traverser< ? super E > > 
+   
+   void traverse(F func)  
+   { 
+      traverse(func, mRoot, 0); 
+   }
+   
+   public FHtreeNode<E> addChild( FHtreeNode<E> treeNode,  E x )
+   {
+      // empty tree? - create a root node if user passes in null
+      if (mSize == 0)
+      {
+         if (treeNode != null)
+            return null; // error something's fishy.  treeNode can't right
+         mRoot = new FHtreeNode<E>(x, null, null, null);
+         mRoot.myRoot = mRoot;
+         mSize = 1;
+         return mRoot;
+      }
+      if (treeNode == null)
+         return null; // error inserting into non_null tree with a null parent
+      if (treeNode.myRoot != mRoot)
+         return null;  // silent error, node does not belong to this tree
+
+      // push this node into the head of the sibling list; adjust prev pointers
+      FHtreeNode<E> newNode = new FHtreeNode<E>(x, 
+         treeNode.firstChild, null, treeNode, mRoot);  // sb, chld, prv, rt
+      treeNode.firstChild = newNode;
+      if (newNode.sib != null)
+         newNode.sib.prev = newNode;
+      ++mSize;
+      return newNode;  
+   }
+   
+   public FHtreeNode<E> find(FHtreeNode<E> root, E x, int level)
+   {
+      FHtreeNode<E> retval;
+
+      if (mSize == 0 || root == null)
+         return null;
+
+      if (root.data.equals(x))
+         return root;
+
+      // otherwise, recurse.  don't process sibs if this was the original call
+      if ( level > 0 && (retval = find(root.sib, x, level)) != null )
+         return retval;
+      return find(root.firstChild, x, ++level);
+   }
+   
+   public boolean remove(FHtreeNode<E> root, E x)
+   {
+      FHtreeNode<E> tn = null;
+
+      if (mSize == 0 || root == null)
+         return false;
+     
+      if ( (tn = find(root, x, 0)) != null )
+      {
+         removeNode(tn);
+         mSize--;
+         return true;
+      }
+      return false;
+   }
+   
+   private void removeNode(FHtreeNode<E> nodeToDelete )
+   {
+      if (nodeToDelete == null || mRoot == null)
+         return;
+      if (nodeToDelete.myRoot != mRoot)
+         return;  // silent error, node does not belong to this tree
+
+      // remove all the children of this node
+      while (nodeToDelete.firstChild != null)
+         removeNode(nodeToDelete.firstChild);
+
+      if (nodeToDelete.prev == null)
+         mRoot = null;  // last node in tree
+      else if (nodeToDelete.prev.sib == nodeToDelete)
+         nodeToDelete.prev.sib = nodeToDelete.sib; // adjust left sibling
+      else
+         nodeToDelete.prev.firstChild = nodeToDelete.sib;  // adjust parent
+
+      // adjust the successor sib's prev pointer
+      if (nodeToDelete.sib != null)
+         nodeToDelete.sib.prev = nodeToDelete.prev;
+   }
+   
+   public Object clone() throws CloneNotSupportedException
+   {
+      FHtree<E> newObject = (FHtree<E>)super.clone();
+      newObject.clear();  // can't point to other's data
+
+      newObject.mRoot = cloneSubtree(mRoot);
+      newObject.mSize = mSize;
+      newObject.setMyRoots(newObject.mRoot);
+      
+      return newObject;
+   }
+   
+   private FHtreeNode<E> cloneSubtree(FHtreeNode<E> root)
+   {
+      FHtreeNode<E> newNode;
+      if (root == null)
+         return null;
+
+      // does not set myRoot which must be done by caller
+      newNode = new FHtreeNode<E>
+      (
+         root.data, 
+         cloneSubtree(root.sib), cloneSubtree(root.firstChild),
+         null
+      );
+      
+      // the prev pointer is set by parent recursive call ... this is the code:
+      if (newNode.sib != null)
+         newNode.sib.prev = newNode;
+      if (newNode.firstChild != null)
+         newNode.firstChild.prev = newNode;
+      return newNode;
+   }
+   
+   // recursively sets all myRoots to mRoot
+   private void setMyRoots(FHtreeNode<E> treeNode)
+   {
+      if (treeNode == null)
+         return;
+
+      treeNode.myRoot = mRoot;
+      setMyRoots(treeNode.sib);
+      setMyRoots(treeNode.firstChild);
+   }
+   
+   // define this as a static member so recursive display() does not need
+   // a local version
+   final static String blankString = "                                    ";
+   
+   // let be public so client can call on subtree
+   public void  display(FHtreeNode<E> treeNode, int level) 
+   {
+      String indent;
+
+      // stop runaway indentation/recursion
+      if  (level > (int)blankString.length() - 1)
+      {
+         System.out.println( blankString + " ... " );
+         return;
+      }
+      
+      if (treeNode == null)
+         return;
+
+      indent = blankString.substring(0, level);
+
+      // pre-order processing done here ("visit")
+      System.out.println( indent + treeNode.data ) ;
+      
+      // recursive step done here
+      display( treeNode.firstChild, level + 1 );
+      if (level > 0 )
+         display( treeNode.sib, level );
+   }
+      
+   // often helper of typical public version, but also callable by on subtree
+   public <F extends Traverser<? super E>> 
+   void traverse(F func, FHtreeNode<E> treeNode, int level)
+   {
+      if (treeNode == null)
+         return;
+
+      func.visit(treeNode.data);
+      
+      // recursive step done here
+      traverse( func, treeNode.firstChild, level + 1);
+      if (level > 0 )
+         traverse( func,  treeNode.sib, level);
+   }
+}
+/* ------------------ RUN -------------
+room
+ table
+  south west leg
+  south east leg
+  north west leg
+  north east leg
+ Miguel the human
+  torso
+   right arm
+    right hand
+     pinky
+     ring finger
+     middle finger
+     index finger
+     thumb
+   left arm
+    left hand
+     pinky
+     ring finger
+     middle finger
+     index finger
+     thumb
+ Lily the canine
+  torso
+   wagging tail
+   spare mutant paw
+   left rear paw
+   right rear paw
+   left front paw
+   right front paw
+room
+ table
+  south west leg
+  south east leg
+  north west leg
+  north east leg
+ Lily the canine
+  torso
+   wagging tail
+   left rear paw
+   right rear paw
+   left front paw
+   right front paw
+
+-------------------------------------------- */
+
+
