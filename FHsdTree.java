9cbf0a9ba65385338196eb4b119eb88c7241156d
LucasGloege <49991682+lucasGloege@users.noreply.github.com>
2020-11-26 01:38:09 -0800

    Work night #1
    My first night of work on this assignment

--- FHsdTree.java
+++ FHsdTree.java
@@ -1,27 +1,58 @@
-public class FHtree<E> implements Cloneable
+public class FHsdTree<E> implements Cloneable
 {
    protected int mSize;
    protected FHsdTreeNode<E> mRoot;
    
-   public FHtree() { clear(); }
-   public boolean empty() { return (mSize == 0); }
-   public int size() { return mSize; }
-   public void clear() { mSize = 0; mRoot = null; }
-   
-   public FHsdTreeNode<E> find(E x) { return find(mRoot, x, 0); }
-   public boolean remove(E x) { return remove(mRoot, x); }
-   public void  display()  { display(mRoot, 0); }
-   
-   public < F extends Traverser< ? super E > > 
-   void traverse(F func)  { traverse(func, mRoot, 0); }
-   
-   public FHsdTreeNode<E> addChild( FHsdTreeNode<E> treeNode,  E x )
+   protected boolean deleted; // true if the node has been removed from the tree
+
+   public FHsdTree()
+   {
+      clear();
+   }
+
+   public boolean empty()
+   {
+      return (mSize == 0);
+   }
+
+   public int size()
+   {
+      return mSize;
+   }
+
+   public void clear()
+   {
+      mSize = 0;
+      mRoot = null;
+   }
+
+   public FHsdTreeNode<E> find(E x)
+   {
+      return find(mRoot, x, 0);
+   }
+
+   public boolean remove(E x)
+   {
+      return remove(mRoot, x); 
+   }
+
+   public void display()
+   {
+      display(mRoot, 0);
+   }
+
+   public <F extends Traverser<? super E>> void traverse(F func)
+   {
+      traverse(func, mRoot, 0);
+   }
+
+   public FHsdTreeNode<E> addChild(FHsdTreeNode<E> treeNode, E x)
    {
       // empty tree? - create a root node if user passes in null
       if (mSize == 0)
       {
          if (treeNode != null)
-            return null; // error something's fishy.  treeNode can't right
+            return null; // error something's fishy. treeNode can't right
          mRoot = new FHsdTreeNode<E>(x, null, null, null);
          mRoot.myRoot = mRoot;
          mSize = 1;
@@ -30,18 +61,17 @@ public class FHtree<E> implements Cloneable
       if (treeNode == null)
          return null; // error inserting into non_null tree with a null parent
       if (treeNode.myRoot != mRoot)
-         return null;  // silent error, node does not belong to this tree
+         return null; // silent error, node does not belong to this tree
 
       // push this node into the head of the sibling list; adjust prev pointers
-      FHsdTreeNode<E> newNode = new FHsdTreeNode<E>(x, 
-         treeNode.firstChild, null, treeNode, mRoot);  // sb, chld, prv, rt
+      FHsdTreeNode<E> newNode = new FHsdTreeNode<E>(x, treeNode.firstChild, null, treeNode, mRoot); // sb, chld, prv, rt
       treeNode.firstChild = newNode;
       if (newNode.sib != null)
          newNode.sib.prev = newNode;
       ++mSize;
-      return newNode;  
+      return newNode;
    }
-   
+
    public FHsdTreeNode<E> find(FHsdTreeNode<E> root, E x, int level)
    {
       FHsdTreeNode<E> retval;
@@ -52,20 +82,20 @@ public class FHtree<E> implements Cloneable
       if (root.data.equals(x))
          return root;
 
-      // otherwise, recurse.  don't process sibs if this was the original call
-      if ( level > 0 && (retval = find(root.sib, x, level)) != null )
+      // otherwise, recurse. don't process sibs if this was the original call
+      if (level > 0 && (retval = find(root.sib, x, level)) != null)
          return retval;
       return find(root.firstChild, x, ++level);
    }
-   
+
    public boolean remove(FHsdTreeNode<E> root, E x)
    {
       FHsdTreeNode<E> tn = null;
 
       if (mSize == 0 || root == null)
          return false;
-     
-      if ( (tn = find(root, x, 0)) != null )
+
+      if ((tn = find(root, x, 0)) != null)
       {
          removeNode(tn);
          mSize--;
@@ -73,42 +103,42 @@ public class FHtree<E> implements Cloneable
       }
       return false;
    }
-   
-   private void removeNode(FHsdTreeNode<E> nodeToDelete )
+
+   private void removeNode(FHsdTreeNode<E> nodeToDelete)
    {
       if (nodeToDelete == null || mRoot == null)
          return;
       if (nodeToDelete.myRoot != mRoot)
-         return;  // silent error, node does not belong to this tree
+         return; // silent error, node does not belong to this tree
 
       // remove all the children of this node
       while (nodeToDelete.firstChild != null)
          removeNode(nodeToDelete.firstChild);
 
       if (nodeToDelete.prev == null)
-         mRoot = null;  // last node in tree
+         mRoot = null; // last node in tree
       else if (nodeToDelete.prev.sib == nodeToDelete)
          nodeToDelete.prev.sib = nodeToDelete.sib; // adjust left sibling
       else
-         nodeToDelete.prev.firstChild = nodeToDelete.sib;  // adjust parent
+         nodeToDelete.prev.firstChild = nodeToDelete.sib; // adjust parent
 
       // adjust the successor sib's prev pointer
       if (nodeToDelete.sib != null)
          nodeToDelete.sib.prev = nodeToDelete.prev;
    }
-   
+
    public Object clone() throws CloneNotSupportedException
    {
-      FHtree<E> newObject = (FHtree<E>)super.clone();
-      newObject.clear();  // can't point to other's data
+      FHsdTree<E> newObject = (FHsdTree<E>) super.clone();
+      newObject.clear(); // can't point to other's data
 
       newObject.mRoot = cloneSubtree(mRoot);
       newObject.mSize = mSize;
       newObject.setMyRoots(newObject.mRoot);
-      
+
       return newObject;
    }
-   
+
    private FHsdTreeNode<E> cloneSubtree(FHsdTreeNode<E> root)
    {
       FHsdTreeNode<E> newNode;
@@ -116,13 +146,8 @@ public class FHtree<E> implements Cloneable
          return null;
 
       // does not set myRoot which must be done by caller
-      newNode = new FHsdTreeNode<E>
-      (
-         root.data, 
-         cloneSubtree(root.sib), cloneSubtree(root.firstChild),
-         null
-      );
-      
+      newNode = new FHsdTreeNode<E>(root.data, cloneSubtree(root.sib), cloneSubtree(root.firstChild), null);
+
       // the prev pointer is set by parent recursive call ... this is the code:
       if (newNode.sib != null)
          newNode.sib.prev = newNode;
@@ -130,7 +155,7 @@ public class FHtree<E> implements Cloneable
          newNode.firstChild.prev = newNode;
       return newNode;
    }
-   
+
    // recursively sets all myRoots to mRoot
    private void setMyRoots(FHsdTreeNode<E> treeNode)
    {
@@ -141,49 +166,48 @@ public class FHtree<E> implements Cloneable
       setMyRoots(treeNode.sib);
       setMyRoots(treeNode.firstChild);
    }
-   
+
    // define this as a static member so recursive display() does not need
    // a local version
    final static String blankString = "                                    ";
-   
+
    // let be public so client can call on subtree
-   public void  display(FHsdTreeNode<E> treeNode, int level) 
+   public void display(FHsdTreeNode<E> treeNode, int level)
    {
       String indent;
 
       // stop runaway indentation/recursion
-      if  (level > (int)blankString.length() - 1)
+      if (level > (int) blankString.length() - 1)
       {
-         System.out.println( blankString + " ... " );
+         System.out.println(blankString + " ... ");
          return;
       }
-      
+
       if (treeNode == null)
          return;
 
       indent = blankString.substring(0, level);
 
       // pre-order processing done here ("visit")
-      System.out.println( indent + treeNode.data ) ;
-      
+      System.out.println(indent + treeNode.data);
+
       // recursive step done here
-      display( treeNode.firstChild, level + 1 );
-      if (level > 0 )
-         display( treeNode.sib, level );
+      display(treeNode.firstChild, level + 1);
+      if (level > 0)
+         display(treeNode.sib, level);
    }
-      
+
    // often helper of typical public version, but also callable by on subtree
-   public <F extends Traverser<? super E>> 
-   void traverse(F func, FHsdTreeNode<E> treeNode, int level)
+   public <F extends Traverser<? super E>> void traverse(F func, FHsdTreeNode<E> treeNode, int level)
    {
       if (treeNode == null)
          return;
 
       func.visit(treeNode.data);
-      
+
       // recursive step done here
-      traverse( func, treeNode.firstChild, level + 1);
-      if (level > 0 )
-         traverse( func,  treeNode.sib, level);
+      traverse(func, treeNode.firstChild, level + 1);
+      if (level > 0)
+         traverse(func, treeNode.sib, level);
    }
 }
--- Foothill.java
+++ Foothill.java
@@ -7,9 +7,10 @@ public class Foothill
    // -------  main --------------
    public static void main(String[] args) throws Exception
    {
-      FHtree<String> sceneTree = new FHtree<String>();
+      FHsdTree<String> sceneTree = new FHsdTree<String>();
       FHsdTreeNode<String> tn;
       
+      System.out.println("Starting tree empty? " + sceneTree.empty() + "\n");
       // create a scene in a room
       tn = sceneTree.addChild(null, "room");
 
@@ -58,59 +59,38 @@ public class Foothill
       sceneTree.addChild(tn, "south east leg");
       sceneTree.addChild(tn, "south west leg");
 
+      System.out.println("\n------------ Loaded Tree ----------------- \n");
       sceneTree.display();
       
       sceneTree.remove("spare mutant paw");
       sceneTree.remove("Miguel the human");
       sceneTree.remove("an imagined higgs boson");
       
+      System.out.println("\n------------ Virtual (soft) Tree --------------- \n");
+      sceneTree.display();
+
+      System.out.println("\n----------- Physical (hard) Display ------------- \n");
+      sceneTree.displayPhysical();
+      
+      System.out.println("------- Testing Sizes (compare with above) -------- \n");
+      System.out.println("virtual (soft) size: " + sceneTree.size()  );
+      System.out.println("physiical (hard) size: " + sceneTree.sizePhysical()  );
+
+      System.out.println("------------ Collecting Garbage ---------------- \n");
+      System.out.println("found soft-deleted nodes? " 
+            + sceneTree.collectGarbage()  );
+      System.out.println("immediate collect again? " 
+            + sceneTree.collectGarbage()  );
+
+      System.out.println("--------- Hard Display after garb col ------------ \n");
+
+      sceneTree.displayPhysical();
+
+      System.out.println("Semi-deleted tree empty? " + sceneTree.empty() + "\n");
+      sceneTree.remove("room");
+      System.out.println("Completely-deleted tree empty? " + sceneTree.empty() 
+         + "\n");
+      
       sceneTree.display();
    }
 }
-
-/* ------------------ RUN -------------
-room
- table
-  south west leg
-  south east leg
-  north west leg
-  north east leg
- Miguel the human
-  torso
-   right arm
-    right hand
-     pinky
-     ring finger
-     middle finger
-     index finger
-     thumb
-   left arm
-    left hand
-     pinky
-     ring finger
-     middle finger
-     index finger
-     thumb
- Lily the canine
-  torso
-   wagging tail
-   spare mutant paw
-   left rear paw
-   right rear paw
-   left front paw
-   right front paw
-room
- table
-  south west leg
-  south east leg
-  north west leg
-  north east leg
- Lily the canine
-  torso
-   wagging tail
-   left rear paw
-   right rear paw
-   left front paw
-   right front paw
-
--------------------------------------------- */
